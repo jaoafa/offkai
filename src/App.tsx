65912bace3aeaf1dd9fecdc1e89b5b74344634f0
Hiratake <hiratake@hiratake.xyz>
2022-02-27 23:02:22 +0900

    build: routerを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,20 @@
 // App
 
-import { css } from '@emotion/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Page as IndexPage } from '@/pages/index'
+import { Page as NotFoundPage } from '@/pages/404'
 
 // ----------------------------------------
 // JSX
 // ----------------------------------------
 
 export const App = () => {
-  return <div css={rootStyle}></div>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<IndexPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
-// ----------------------------------------
-// Styles
-// ----------------------------------------
-
-const rootStyle = css`
-  box-sizing: border-box;
-  padding: 0;
-  margin: 0;
-`
